23208475fe02d804dbdd73bc4b900161b7fbc71e
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-06-01 23:05:22 +0200

    add 10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputValueRef = document.querySelector('#controls input');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const boxesContainerRef = document.querySelector('#boxes');
+
+createBtnRef.addEventListener('click', addBoxes);
+destroyBtnRef.addEventListener('click', removeBoxes);
+
+function addBoxes() {
+  createBoxes(inputValueRef.value);
+}
+
+function createBoxes(amount) {
+  const boxesEl = [];
+  let divSize = 30;
+  for (let index = 0; index < amount; index++) {
+    const newBoxEl = document.createElement('div');
+    newBoxEl.style.width = `${divSize}px`;
+    newBoxEl.style.height = `${divSize}px`;
+    newBoxEl.style.backgroundColor = getRandomHexColor();
+    boxesEl.push(newBoxEl);
+    divSize += 10;
+  }
+  boxesContainerRef.append(...boxesEl);
+}
+
+function removeBoxes() {
+  boxesContainerRef.innerHTML = '';
+  // inputValueRef.value = ''; // Очистка инпута
+}
